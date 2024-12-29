1017a8179e050b5efc97173d8882f1642ad985f8
musttafauygun <uygunm295@gmail.com>
2024-12-29 19:59:24 +0300

    Rating Bar

---
+++ app/src/main/java/com/example/ytratingbar/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ytratingbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ytratingbar.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        EdgeToEdge.enable(this);
+        setContentView(view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.ratingBar2.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean b) {
+                binding.tvRating.setText(String.valueOf(rating));
+            }
+        });
+
+        binding.btnKaydet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float rating = binding.ratingBar2.getRating();
+                binding.tvBtn.setText(String.valueOf(rating));
+            }
+        });
+    }
+}
